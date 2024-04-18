3a06afb9ed90a8d0da6bf456cc2e0dbdaff83dd5
Sayedul Sayem <sayedulsayem@gmail.com>
2024-04-18 23:01:18 +0600

    use fetch over ajax

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <script setup>
 import data from "./data.js";
 // const pluginData = wordpress_vuejs;
-
-jQuery.ajax({
-  url: wordpress_vuejs.ajax_url,
-  type: 'get',
-  data: {
-    action: 'vuejs_data',
-    nonce: wordpress_vuejs.nonce,
-  },
-  dataType: 'json',
-  success(res) {
+fetch(wordpress_vuejs.ajax_url + '?action=vuejs_data&nonce=' + wordpress_vuejs.nonce)
+  .then(res => res.json())
+  .then(res => {
     data.welcome = res.welcome
     data.settings = res.settings
-  },
-  error(req, status, error) {
-    console.log([ req, status, error ]);
-  }
-});
+  })
+  .catch(err => console.log(err));
+
+// jQuery.ajax({
+//   url: wordpress_vuejs.ajax_url,
+//   type: 'get',
+//   data: {
+//     action: 'vuejs_data',
+//     nonce: wordpress_vuejs.nonce,
+//   },
+//   dataType: 'json',
+//   success(res) {
+//     data.welcome = res.welcome
+//     data.settings = res.settings
+//   },
+//   error(req, status, error) {
+//     console.log([ req, status, error ]);
+//   }
+// });
 
 </script>
 
 <template>
-  <img :src="pluginData.plugin_url+'public/vite.svg'" alt="">
+  <img :src="pluginData.plugin_url + 'public/vite.svg'" alt="">
   <router-link to="/">Welcome</router-link>
   <router-link to="/settings">Settings</router-link>
   <router-view></router-view>
